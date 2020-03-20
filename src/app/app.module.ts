94c73174396df6ec2ec62e25488a51ec48f4c975
Elena Frank <elena.frank@cgi.com>
2020-03-21 00:11:45 +0100

    created inital components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {TaskService} from './shared/services/task.service';
+import {HeaderComponent} from './shared/header/header.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // components
+    AppComponent,
+    HomeComponent,
+    // shared
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    // core
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: '**', redirectTo: 'home'},
+    ]),
+    // material design
   ],
-  providers: [],
+  providers: [
+    // services
+    TaskService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
