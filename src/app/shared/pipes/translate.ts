dd81e3c6b8999921d5463c588b25abbe65adcb9c
Elena Frank <elena.frank@cgi.com>
2020-03-21 02:18:03 +0100

    added environment.language language on translation

--- src/app/shared/pipes/translate.ts
+++ src/app/shared/pipes/translate.ts
@@ -1,13 +1,17 @@
 import {Pipe, PipeTransform} from '@angular/core';
+import {environment} from '../../../environments/environment';
 import {DE} from '../../../assets/i18n/de';
+import {EN} from '../../../assets/i18n/en';
 
 @Pipe({
   name: 'translate'
 })
 export class Translate implements PipeTransform {
+
   public transform(key: string): any {
+    const lang = environment.lang === 'de' ? DE : EN;
     const keys = key.split('.');
-    const transl = keys.reduce((acc, curr) => acc[curr], DE);
+    const transl = keys.reduce((acc, curr) => acc[curr], lang);
     return transl ? transl : keys.pop().charAt(0).toUpperCase() + keys.pop().slice(1);
   }
 }
---
+++ src/assets/i18n/en.ts
@@ -0,0 +1,10 @@
+export const EN = {
+  navigation: {
+    home: 'Home',
+    profile: 'Profile',
+  },
+  user: {
+    first_name: 'First Name',
+    last_name: 'Last Name'
+  }
+};
