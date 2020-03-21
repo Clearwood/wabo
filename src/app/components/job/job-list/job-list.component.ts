f8a4fe06bf0b4d07a8b416f207f69a7838e8d7c7
Gianluca Volkmer <gianvolkmer@gmail.com>
2020-03-21 15:13:47 +0100

    First jobslist changes

--- src/app/components/job/job-list/job-list.component.ts
+++ src/app/components/job/job-list/job-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Job } from 'src/app/models/job';
+import { JobService } from 'src/app/shared/services/job.service';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-job-list',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobListComponent implements OnInit {
 
-  constructor() { }
+  private jobs: Job[];
+
+  constructor(
+    private jobService: JobService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  private getJobs() {
+    const params = new HttpParams().set("longitude", "0.0").set("latidue", "0.0").set("supplierId ", "null");
+    this.jobService.getAllJobs(params).subscribe(jobs => {
+      this.jobs = jobs;
+    })
   }
 
 }
